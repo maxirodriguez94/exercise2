37a371acb0e881af465f48aabd45e8773b6f4cac
maxi <maxi.rodriguez94@gmail.com>
2021-07-13 18:05:04 -0300

    correcion linea 7

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 //middleware
  let logUser = function (req, res, next) {
      let user = 'Autorizacion'
-    if (user.req.get('Autorizacion')) {
+    if (user.includes(req.get('Autorizacion'))) {
         res.status(403).send('')
     } else return res.send('test')
 
@@ -13,8 +13,8 @@ const app = express()
 
 app.use(logUser)
 
-app.get('/user', function (req, res) {
-    res.sendFile(driname + '/src/views/user.html')
+app.get('/user', async function (req, res) {
+    res.send('asd')
 })
 
 app.get('/Autorizacion', async function (req, res) {
@@ -23,4 +23,4 @@ res.send('asd')
 
 })
 
-app.listen(5001)
+app.listen(5004)
